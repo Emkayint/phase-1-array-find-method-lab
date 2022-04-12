d331c9c32842d90b4159edd1fdaa377461a8260e
Martin <mkmartiones@gmail.com>
2022-04-12 09:48:02 +0300

    Finished Assignment

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-// function superbowlWin(data){
-//     let won = data.find(function(param){
-//         if(param.result === "W"){
-//             let results =  param.year
-//             return results
-//         }
-//     })
-//     return won.year
-// }
-
-
 function superbowlWin(data){
-    let won;
+    let won= [];
     for(let collection of data){
         if(collection.result === "W"){
-            won = '2015'
+            won.push(collection.year)
         }
     }
-    return won
+    return won[0]
 }
 
+console.log(superbowlWin(record))
